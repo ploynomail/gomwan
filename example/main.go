754ff90e58d23dabd2f19ba8c6de2a58dba4c544
hongmin.zhang <cyrus_zhang@163.com>
2024-11-28 07:11:30 +0000

    prefact example

--- example/main.go
+++ example/main.go
@@ -53,7 +53,46 @@ func main() {
 	}
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	go mwan.TicketMainTain(ctx)
-	time.Sleep(600 * time.Second)
+	time.Sleep(60 * time.Second)
+
+	variable, err = gomwan.NewVariable(
+		gomwan.Interfaces{
+			{
+				Name:          "eth1",
+				InitialWeight: 0.2,
+				InfType:       gomwan.WAN,
+				IP:            net.ParseIP("192.168.22.224"),
+				Mask:          net.CIDRMask(23, 32),
+				Gateway:       net.ParseIP("192.168.22.1"),
+				Primary:       false,
+			},
+			{
+				Name:          "eth3",
+				InitialWeight: 0.8,
+				InfType:       gomwan.WAN,
+				IP:            net.ParseIP("192.168.1.100"),
+				Mask:          net.CIDRMask(24, 32),
+				Gateway:       net.ParseIP("192.168.1.1"),
+				Primary:       true,
+			},
+			{
+				Name:    "eth5",
+				InfType: gomwan.LAN,
+				IP:      net.ParseIP("192.168.2.0"),
+				Mask:    net.CIDRMask(24, 32),
+			},
+		}, gomwan.Weight, []net.IP{
+			net.ParseIP("192.168.23.73"),
+		},
+	)
+	if err != nil {
+		fmt.Println("variable", err)
+	}
+	mwan.SetVariable(variable)
+	if err := mwan.TriggerChagne(); err != nil {
+		fmt.Println("trigger change", err)
+	}
+	time.Sleep(60 * time.Second)
 	cancelFunc()
 	if err := mwan.Destory(); err != nil {
 		panic(err)
